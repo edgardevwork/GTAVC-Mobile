92df664b67ccd143fad1fca04399c61d4ebc5dd8
EDGAR <edgarworkdev@gmail.com>
2025-10-15 03:47:00 +0300

    FIX touch crash

---
+++ README.md
@@ -0,0 +1,19 @@
+# ᐳ GTAVC-Source for GTA: Vice City Android
+
+## ᐳ Description
+> * **AArch**: `armeabi-v7a`, `arm64-v8a`  
+> * **Platform**: `Android NDK`  
+> * **Security / Hooking**: `DobbyHook`, `ShadowHook`  
+> * **Status**: Work in Progress (WIP)
+
+## ᐳ Features
+> * Reverse-engineered RenderWare engine components: `RwWorld`, `RpSkin`, `RwCore`, `rphanim`, and more  
+> * Game loop, camera, script handling  
+> * Hooking via `CHooks`, `ARMHook`  
+> * Native integration through `JNI_OnLoad`  
+> * TODO: Online multiplayer support via plugin
+
+## ᐳ Credits
+> - [**EDGAR**](https://github.com/edgardevwork) — Main author
+> - [**DobbyHook**](https://github.com/jmpews/Dobby) — Lightweight inline hook framework  
+> - [**ShadowHook**](https://github.com/bytedance/android-inline-hook) — Android function hooking library by ByteDance
---
+++ app/src/main/cpp/jni/CHooks.cpp
@@ -0,0 +1,62 @@
+#include "main.h"
+#include "CHooks.h"
+#include <iostream>
+
+void (*CMenuManager_ProcessButtonPresses)(uintptr_t);
+void CMenuManager_ProcessButtonPresses_hook(uintptr_t thiz)
+{
+    //LOGI("libGTAVC.so: Patching the game...")
+    return CMenuManager_ProcessButtonPresses(thiz);
+}
+void (*CMenuManager_DrawLoadingScreen)(uintptr_t);
+void CMenuManager_DrawLoadingScreen_hook(uintptr_t thiz)
+{
+    return CMenuManager_DrawLoadingScreen(thiz);
+}
+
+
+int (*OS_ServiceOpenLink)(const char* url);
+int OS_ServiceOpenLink_hook(const char* url) {
+    const char* newUrl = MAKEOBF("https://t.me/edgardevwork/");
+    if (strstr(url, MAKEOBF("http://www.rockstarwarehouse.com/"))) {
+        return OS_ServiceOpenLink(newUrl);
+    }
+    return OS_ServiceOpenLink(url);
+}
+
+void (*LoadingScreen)(const char *, const char *, const char *);
+void LoadingScreen_hook(const char *text, const char *text1, const char *text2)
+{
+    LOGI(MAKEOBF("text %s"), text);
+    LOGI(MAKEOBF("text %s"), text1);
+    LOGI(MAKEOBF("text %s"), text);
+    return LoadingScreen(text, text1, text2);
+}
+void (*CHud_Draw)(uintptr_t);
+void CHud_Draw_hook(uintptr_t thiz)
+{
+    //LOGI("libGTAVC.so: Patching the game...");
+    return;
+}
+
+//#include "game/hooks.h"
+#include "dependencies/Dobby/include/dobby.h"
+void CHooks::InitHooksEdgar() {
+
+    #ifdef IS_ARM64
+    DobbyHook((void *)GTA(0x206490), (void *)CMenuManager_ProcessButtonPresses_hook, (void **)&CMenuManager_ProcessButtonPresses);
+    DobbyHook((void *)GTA(0x201F00), (void *)CMenuManager_DrawLoadingScreen_hook, (void **)&CMenuManager_DrawLoadingScreen);
+    DobbyHook((void *)GTA(0x1F8CC8), (void *)LoadingScreen_hook, (void **)&LoadingScreen);
+    DobbyHook((void *)GTA(0x2C317C), (void *)CHud_Draw_hook, (void **)&CHud_Draw);
+    //DobbyHook((void *)GTA(0x45CE04), (void *)NvUtilInit_hook, (void **)NvUtilInit);
+    DobbyHook((void *)GTA(0x4609CC), (void *)OS_ServiceOpenLink_hook, (void **)&OS_ServiceOpenLink);
+
+    #elif defined(IS_ARM32)
+    DobbyHook((void *)GTA(0x155848), (void *)CMenuManager_ProcessButtonPresses_hook, (void **)&CMenuManager_ProcessButtonPresses);
+    DobbyHook((void *)GTA(0x152070), (void *)CMenuManager_DrawLoadingScreen_hook, (void **)&CMenuManager_DrawLoadingScreen);
+    DobbyHook((void *)GTA(0x14E164), (void *)LoadingScreen_hook, (void **)&LoadingScreen);
+    //DobbyHook((void *)GTA(0x1E8278), (void *)CHud_Draw_hook, (void **)&CHud_Draw);
+    //DobbyHook((void *)GTA(0x3AC59C), (void *)NvUtilInit_hook, (void **)NvUtilInit);
+    DobbyHook((void *)GTA(0x322D18), (void *)OS_ServiceOpenLink_hook, (void **)&OS_ServiceOpenLink);
+    #endif
+}
